711d14aea4315e2a2e15cf8515fae2a354ba44f7
RITIK POONIA <poonia2907@gmail.com>
2026-01-04 02:04:04 +0530

    update

--- src/main/java/in/ritikpoonia/resumateapi/config/SecurityConfig.java
+++ src/main/java/in/ritikpoonia/resumateapi/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://resumate-frontend-2m6j.onrender.com"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
