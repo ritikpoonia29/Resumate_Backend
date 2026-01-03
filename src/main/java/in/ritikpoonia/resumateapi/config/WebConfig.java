0333996cc21ecbc3449526eba66051e7385710b4
RITIK POONIA <poonia2907@gmail.com>
2026-01-04 01:18:44 +0530

    Update

--- src/main/java/in/ritikpoonia/resumateapi/config/WebConfig.java
+++ src/main/java/in/ritikpoonia/resumateapi/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://resumate-frontend-2m6j.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
